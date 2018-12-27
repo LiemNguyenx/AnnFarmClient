1fe0b7433be89ed7b7c5ca0d0ec467ab486bbf91
Liem <liemnguyenx@gmail.com>
2018-12-27 13:01:20 +0700

    ckeditor

--- src/js/components/admin/containers/contents.jsx
+++ src/js/components/admin/containers/contents.jsx
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+
 import TopNavbar from '../menu/top-navbar.jsx'
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
@@ -60,6 +69,19 @@ class Contents extends Component {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="editor">
+                        <h2>Using CKEditor 5 Framework in React</h2>
+                        <CKEditor
+                            onInit={editor => console.log('Editor is ready to use!', editor)}
+                            onChange={(event, editor) => console.log({ event, editor })}
+                            config={{
+                                plugins: [Essentials, Paragraph, Bold, Italic, Heading],
+                                toolbar: ['heading', '|', 'bold', 'italic', '|', 'undo', 'redo',]
+                            }}
+                            editor={ClassicEditor}
+                            data="<p>Hello from CKEditor 5!</p>"
+                        />
                     </div>
                 </div>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
 
 module.exports = {
     // entry: './src/js/index.js',
@@ -25,7 +27,49 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader'],
+                exclude: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                use: ['raw-loader']
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: styles.getPostCssConfig({
+                            themeImporter: {
+                                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                            },
+                            minify: true
+                        })
+                    }
+                ]
+            },
+            {
+                loader: require.resolve('file-loader'),
+                // Exclude `js` files to keep the "css" loader working as it injects
+                // its runtime that would otherwise be processed through the "file" loader.
+                // Also exclude `html` and `json` extensions so they get processed
+                // by webpack's internal loaders.
+                exclude: [
+                    /\.(js|jsx|mjs)$/,
+                    /\.html$/,
+                    /\.json$/,
+                    /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                    /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/
+                ],
+                options: {
+                    name: 'static/media/[name].[hash:8].[ext]'
+                }
             }
         ]
     },
@@ -33,6 +77,28 @@ module.exports = {
         new HtmlWebPackPlugin({
             // template: './src/index.html'
             template: './src/js/components/admin/index.html'
+        }),
+        new UglifyJsWebpackPlugin({
+            uglifyOptions: {
+                compress: {
+                    warnings: false,
+                    // Disabled because of an issue with Uglify breaking seemingly valid code:
+                    // https://github.com/facebookincubator/create-react-app/issues/2376
+                    // Pending further investigation:
+                    // https://github.com/mishoo/UglifyJS2/issues/2011
+                    comparisons: false,
+                },
+                mangle: {
+                    safari10: true,
+                },
+                output: {
+                    comments: false,
+                    // Turned on because emoji and regex is not minified properly using default
+                    // https://github.com/facebookincubator/create-react-app/issues/2488
+                    ascii_only: true,
+                },
+            },
+            // sourceMap: shouldUseSourceMap,
         })
     ]
 }
